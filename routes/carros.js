85c474141222da435fb3f5b77d2e4882175a7e4d
Leo Picado <lpicado@steals.com>
2015-03-22 12:27:37 -0600

    Commit inicial. End points: - GET: /api/carros/ - GET: /api/carros/:id

---
+++ routes/carros.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('underscore');
+var cors = require('cors');
+
+// DB en memoria
+var marcas = ["Volkswagen", "Nissan", "Toyota", "Honda", "Hyundai", "Lexus", "Mitsubishi"],
+    modelos = ["Crossfox", "Tida", "Corolla", "Accord", "Accent", "Yaris", "Beetle", "Amarok", "Lancer"];
+var carros = [];
+_.times(5, function() {
+    _.each(marcas, function(marca) {
+        carros.push({
+            "id": carros.length + 1,
+            "marca": marca,
+            "modelo": _.sample(modelos)
+        });
+    });
+});
+
+var _buscarPorId = function(id) {
+    return _.find(carros, function(carro) {
+        return carro.id == id;
+    });
+}
+
+// GET lista
+router.get('/', cors(), function(req, res, next) {
+    res.status(200).send(carros);
+});
+
+// GET unico
+router.get('/:id', cors(), function(req, res, next) {
+    var carro = _buscarPorId(req.params.id);
+    carro ? res.send(carro) : res.sendStatus(404);
+});
+
+module.exports = router;
